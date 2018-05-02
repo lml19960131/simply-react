90507469347985ce73490fb766450deb23833137
lml19960131 <lml19960131@126.com>
2018-05-02 22:10:25 +0800

    simply-react

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const React = {
+  createElement
+}
+
+const ReactDOM = {
+  render: (vnode, container) => {
+    container.innerHTML = '';
+    return render(vnode, container)
+  }
+}
+
 function createElement(tag, attrs, ...children) {
   return {
     tag,
@@ -6,14 +17,60 @@ function createElement(tag, attrs, ...children) {
   }
 }
 
-const React = {
-  createElement
+function render(vnode, container) {
+  if (typeof vnode === 'string') {
+    let textNode = document.createTextNode(vnode);
+    return container.appendChild(textNode);
+  }
+  const dom = document.createElement(vnode.tag);
+  if (vnode.attrs) {
+    Object.keys(vnode.attrs).forEach(key => {
+      const value = vnode.attrs[key];
+      setAttribute(dom, key, value)
+    })
+  }
+  vnode.children.forEach(child => render(child, dom));
+  return container.appendChild(dom);
 }
 
-const element = (
-  <div>
-    hello<span>world !</span>
-  </div>
-)
+function setAttribute(dom, name, value) {
+  if (name === 'className') {
+    name = 'class'
+  }
+  if (/on\w+/.test(name)) {
+    name = name.toLowerCase();
+    dom[name] = value || ''
+  } else if (name === 'style') {
+    if (!value || typeof value === 'string') {
+      node.style.cssText = value || '';
+    } else if (value && typeof value === 'object') {
+      for (let name in value) {
+        dom.style[name] = typeof value[name] === 'number' ? value[name] + 'px' : value[name];
+      }
+    } else {
+      if (name in dom) {
+        dom[name] = value || '';
+      }
+      if (value) {
+        dom.setAttribute(name, value);
+      } else {
+        dom.removeAttribute(name, value)
+      }
+    }
+  }
+}
 
-console.log(element)
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
+}
+
+setInterval(tick, 1000)
